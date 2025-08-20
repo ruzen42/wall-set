1a1b9aa6773b841bdfe2a0b1976e8403e5b03ac4
ruzen42 <malledeferlive@gmail.com>
2025-08-21 01:43:17 +0500

    1.0.0.0 init version with minimal features

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
-module Main (main) where
+module Main where
 
-import Lib
+import BackgroundSet (backgroundSet)
+import Options.Applicative
+import System.Exit (exitFailure)
+import Control.Monad (when)
+
+data Options = Options
+  { optPath :: FilePath  
+  , optVerbose :: Bool  
+  } deriving (Show)
+
+optionsParser :: Parser Options
+optionsParser = Options
+  <$> argument str (metavar "PATH" <> help "Path to file or directory with backgrounds")
+  <*> switch (long "verbose" <> short 'v' <> help "Enable verbose output")
+
+programInfo :: ParserInfo Options
+programInfo = info (optionsParser <**> helper)
+  ( fullDesc
+  <> progDesc "Set random background from directory or specific file"
+  <> header "background-set - utility for setting desktop backgrounds" )
 
 main :: IO ()
-main = someFunc
+main = do
+  opts <- execParser programInfo
+  let path = optPath opts
+      verbose = optVerbose opts
+  
+  when verbose $ putStrLn $ "Processing path: " ++ path
+  
+  result <- backgroundSet path
+  
+  case result of
+    Right () -> when verbose $ putStrLn "Background set successfully!"
+    Left err -> do
+      putStrLn $ "Error: " ++ err
+      exitFailure
